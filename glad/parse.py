f68cef5f926c2c9dbba7d029c1ce7c335963bddd
Steven Noonan <steven@uplinklabs.net>
2024-03-05 21:26:32 -0800

    parse: use sorted list for protections to ensure consistent output
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- glad/parse.py
+++ glad/parse.py
@@ -565,7 +565,7 @@ class Specification(object):
                     # symbol found in at least one unprotected extension
                     return list()
 
-        return list(set(protections))
+        return sorted(list(set(protections)))
 
     def find(self, require, api, profile, recursive=False):
         """
